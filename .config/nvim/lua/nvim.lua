d13fb527558562a1ed069876223d7f11704fb485
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-13 15:29:02 -0700

    Move a some options to nvim.lua from vimrc.vim (.vimrc)

---
+++ .config/nvim/lua/nvim.lua
@@ -0,0 +1,11 @@
+local options = {
+  number = true,            -- set numbered lines
+  relativenumber = true,    -- set relative numbered lines
+  termguicolors = true,     -- set terminal GUI colors
+}
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
+end
+
+vim.cmd("set iskeyword+=-")
