97b6ae6ad55262bf99384d7f380c23accec465cd
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-13 18:21:56 -0700

    Use double quotes instead of single quotes

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,21 +1,22 @@
-return require('packer').startup(function(use)
-  use 'gruvbox-community/gruvbox'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/vim-vsnip'
-  use 'itchyny/lightline.vim'
-  use 'jalvesaq/Nvim-R'
-  use 'mengelbrecht/lightline-bufferline'
-  use 'neovim/nvim-lspconfig'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'wbthomason/packer.nvim'
-  use 'williamboman/nvim-lsp-installer'
+local use = require("packer").use
+require("packer").startup(function()
+  use "gruvbox-community/gruvbox"          --
+  use "hrsh7th/cmp-buffer"                 --
+  use "hrsh7th/cmp-cmdline"                --
+  use "hrsh7th/cmp-nvim-lsp"               --
+  use "hrsh7th/cmp-path"                   --
+  use "hrsh7th/cmp-vsnip"                  --
+  use "hrsh7th/nvim-cmp"                   --
+  use "hrsh7th/vim-vsnip"                  --
+  use "itchyny/lightline.vim"              --
+  use "jalvesaq/Nvim-R"                    --
+  use "mengelbrecht/lightline-bufferline"  --
+  use "neovim/nvim-lspconfig"              -- Collection of configurations for the built-in LSP client
+  use "nvim-lua/plenary.nvim"              --
+  use "nvim-telescope/telescope.nvim"      --
+  use "nvim-treesitter/nvim-treesitter"    --
+  use "wbthomason/packer.nvim"             -- Package manage
+  use "williamboman/nvim-lsp-installer"    --
 
-  require('packer').sync()
+  require("packer").sync()
 end)
