ba870ad0d4bfb8214584a342b989742817f86115
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-15 09:32:32 -0700

    Move configuratios frim Vimscript to Lua

--- .config/nvim/lua/nvim.lua
+++ .config/nvim/lua/nvim.lua
@@ -1,7 +1,22 @@
 local options = {
+  colorcolumn = "100",
+  expandtab = true,
+  hidden = true,
+  hlsearch = true,
+  incsearch = true,
   number = true,            -- set numbered lines
   relativenumber = true,    -- set relative numbered lines
+  scrolloff = 10,
+  shiftwidth = 4,
+  showmode = false,
+  showtabline = 2,
+  signcolumn = "yes",
+  smartcase = true,
+  swapfile = false,
+  tabstop = 4,
   termguicolors = true,     -- set terminal GUI colors
+  textwidth = 100,
+  wrap = false,
 }
 
 for key, value in pairs(options) do
@@ -9,3 +24,5 @@ for key, value in pairs(options) do
 end
 
 vim.opt["iskeyword"]:append("-")
+vim.opt["formatoptions"]:remove("t")
+
--- .config/nvim/vim/vimrc.vim
+++ .config/nvim/vim/vimrc.vim
@@ -6,21 +6,5 @@ nnoremap <Down> <Nop>
 nnoremap <Left> <Nop>
 nnoremap <Right> <Nop>
 nnoremap <Up> <Nop>
-set colorcolumn=100
-set expandtab
-set formatoptions-=t
-set hidden
-set hlsearch
-set incsearch
 set list listchars=tab:»·,extends:>,precedes:<,trail:·
-set noshowmode
-set noswapfile
-set nowrap
-set scrolloff=10
-set shiftwidth=4
-set showtabline=2
-set signcolumn=yes
-set smartcase
-set tabstop=4
-set textwidth=100
 syntax on
