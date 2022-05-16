66e4520180e08877476421b060bfb1baaf75cb81
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-15 17:38:36 -0700

    Add keymaps to navigate buffers

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,4 +2,8 @@ local options = { noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
 -- Cuando pegues en visual mode, deja en el registry el valor que pegaste
+keymap("n", "<C-j>", "<cmd>bnext<CR>", options)
+keymap("n", "<C-k>", "<cmd>bprevious<CR>", options)
 keymap("v", "p", '"_dP', options)
+
+
