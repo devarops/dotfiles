534f935da76c8f8cc01615f5a8928a6312d1ce8e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-22 16:39:17 -0700

    Add pair programming configuration

---
+++ .config/nvim/lua/pair_programming.lua
@@ -0,0 +1,12 @@
+local _MODULE = {}
+function _MODULE.pair()
+    vim.opt.relativenumber = false
+    vim.cmd("colorscheme slate")
+end
+
+function _MODULE.solo()
+    vim.opt.relativenumber = true
+    vim.cmd("colorscheme gruvbox")
+end
+
+return _MODULE
