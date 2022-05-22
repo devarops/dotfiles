39e73a31e208864d32ba5c99e17fd911007dbf1a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-22 11:44:20 -0700

    Update LSP configuration

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,11 +1,29 @@
-require("nvim-lsp-installer").setup {}
+require("nvim-lsp-installer").setup({
+    automatic_installation = true
+})
 local servers = {
-  "pyright",
-  "r_language_server",
-  "sumneko_lua",
+    "pyright",
+    "r_language_server",
+    "sumneko_lua",
 }
+
+local function is_file(filename)
+    local file = io.open(filename, "r")
+    if file ~= nil then io.close(file) return true else return false end
+end
+
+local function get_lsp_configuration(lsp)
+    local lsp_config_filename = os.getenv("HOME") .. "/.config/nvim/lua/lsp/" .. lsp .. ".lua"
+    if is_file(lsp_config_filename) then
+        print("Loaded " .. lsp .. " configuration")
+        return require("lsp." .. lsp)
+    end
+    print("No configuration found for " .. lsp)
+    return {}
+end
+
 local lspconfig = require("lspconfig")
 for _, lsp in pairs(servers) do
-  local server_configuration = require("lsp." .. lsp)
-  lspconfig[lsp].setup(server_configuration)
+    local lsp_configuration = get_lsp_configuration(lsp)
+    lspconfig[lsp].setup(lsp_configuration)
 end
