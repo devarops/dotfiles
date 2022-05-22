230cdfb5e84ae973555d37af2d51107b90075e2e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-22 13:39:39 -0700

    Add Telescope key maps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,9 +1,11 @@
-local options = { noremap = true, silent = true}
+local options = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
--- Cuando pegues en visual mode, deja en el registry el valor que pegaste
 keymap("n", "<C-j>", "<cmd>bnext<CR>", options)
 keymap("n", "<C-k>", "<cmd>bprevious<CR>", options)
-keymap("v", "p", '"_dP', options)
-
-
+keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", options)
+keymap("n", "<leader>fc", "<cmd>Telescope command_history<cr>", options)
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", options)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", options)
+keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", options)
+keymap("v", "p", '"_dP', options) -- Cuando pegues en visual mode, deja en el registry el valor que pegaste
