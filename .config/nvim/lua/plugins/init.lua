3ed6dc086e9c165f8909b09c79d81170b434b382
Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>
2023-06-24 21:49:54 -0700

    💤 Use lazy.nvim instead of packer.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,30 +1,43 @@
-local use = require("packer").use
-require("packer").startup(function()
-    use "folke/which-key.nvim"              -- Display available keybindings
-    use "github/copilot.vim"                --
-    use "gruvbox-community/gruvbox"         --
-    use "williamboman/mason.nvim"           --
-    use "williamboman/mason-lspconfig.nvim" --
-    use "hrsh7th/cmp-buffer"    --
-    use "hrsh7th/cmp-cmdline"   --
-    use "hrsh7th/cmp-nvim-lsp"  --
-    use "hrsh7th/cmp-path"      --
-    use "hrsh7th/cmp-vsnip"     --
-    use "hrsh7th/nvim-cmp"      --
-    use "hrsh7th/vim-vsnip"     --
-    use "itchyny/lightline.vim" --
-    use "jalvesaq/Nvim-R"       --
-    use "mengelbrecht/lightline-bufferline" --
-    use "navarasu/onedark.nvim"             -- Theme inspired by Atom
-    use "neovim/nvim-lspconfig"             --
-    use "nvim-lua/plenary.nvim"             -- Dependencie of nvim-telescope/telescope.nvim
-    use "nvim-telescope/telescope.nvim"     --
-    use "nvim-treesitter/nvim-treesitter"   --
-    use "wbthomason/packer.nvim"            -- Plugin manager
-    use "whonore/vim-sentencer"             -- One sentence per line and wrap long lines
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    require("packer").sync()
-end)
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  "folke/which-key.nvim",             -- Display available keybindings
+  "github/copilot.vim",               --
+  "gruvbox-community/gruvbox",        --
+  "williamboman/mason.nvim",          --
+  "williamboman/mason-lspconfig.nvim",--
+  "hrsh7th/cmp-buffer",   --
+  "hrsh7th/cmp-cmdline",  --
+  "hrsh7th/cmp-nvim-lsp", --
+  "hrsh7th/cmp-path",     --
+  "hrsh7th/cmp-vsnip",    --
+  "hrsh7th/nvim-cmp",     --
+  "hrsh7th/vim-vsnip",    --
+  "itchyny/lightline.vim",--
+  "jalvesaq/Nvim-R",      --
+  "mengelbrecht/lightline-bufferline",--
+  "navarasu/onedark.nvim",            -- Theme inspired by Atom
+  "neovim/nvim-lspconfig",            --
+  "nvim-lua/plenary.nvim",            -- Dependencie of nvim-telescope/telescope.nvim
+  "nvim-telescope/telescope.nvim",    --
+  "nvim-treesitter/nvim-treesitter",  --
+  "wbthomason/packer.nvim",           -- Plugin manager
+  "whonore/vim-sentencer",            -- One sentence per line and wrap long lines
+})
 
 require("plugins.treesitter")
 require("plugins.cmp")
