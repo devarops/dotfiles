266e75ffec901a06ccf52b1c4e1ba0a7e50539c3
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-07-10 12:45:31 -0700

    ❄️ Add direnv

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -111,6 +111,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    direnv
     docker
     docker-compose
     exa
