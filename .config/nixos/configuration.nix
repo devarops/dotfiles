34f7d1113f530071f79c07ffb5f9824ac91fc9d2
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-07-10 14:46:31 -0700

    ❄️  Enable flakes

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -108,6 +108,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
