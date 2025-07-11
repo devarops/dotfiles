d5401706c716f5edfbd4a1115feaf50c3328a9ac
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-10 17:20:56 -0700

    ⌨  Remap Telescope search keys from `<leader>s` to `<leader>f`
    Replaced `<leader>sf`, `<leader>sg`, etc., with `<leader>ff`, `<leader>fg`, etc.
    Updated which-key descriptions to reflect the new `[F]ind` prefix for Telescope commands.

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,6 +2,6 @@ local wk = require("which-key")
 wk.setup {}
 wk.register({
   ["<leader>p"] = { name = "[P]rogramming environment" },
-  ["<leader>s"] = { name = "[S]earch using Telescope" },
+  ["<leader>f"] = { name = "[F]ind using Telescope" },
 })
 
