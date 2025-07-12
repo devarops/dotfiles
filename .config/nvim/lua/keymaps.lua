e76aeec719da0c754fb9058617ad7c0044ef9803
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-11 19:35:42 -0700

    ✨ feat(keymaps): map `gd` to `vim.lsp.buf.definition` for direct go-to-definition

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,4 +7,5 @@ vim.keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Fi
 vim.keymap.set("n", "<leader>pp", require("pair_programming").pair, { desc = "[P]air programming" })
 vim.keymap.set("n", "<leader>ps", require("pair_programming").solo, { desc = "[S]olo programming" })
 vim.keymap.set("n", "<leader>pt", require("pair_programming").tty, { desc = "[T]eletypewriter" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("v", "p", '"_dP', { desc = "Leave the pasted string in the registry" })
