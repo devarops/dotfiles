0aad23e20755055bd80ab322ff36be5333a2115d
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-14 21:31:52 -0700

    ✨ add nvim-web-devicons and improve plugin descriptions
    - Add nvim-web-devicons for icon support in which-key and other plugins
    - Sort plugins alphabetically for better organization
    - Add descriptive comments for all plugins to clarify their purpose

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -15,27 +15,28 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-  "folke/which-key.nvim",             -- Display available keybindings
-  "github/copilot.vim",               --
-  "gruvbox-community/gruvbox",        --
-  "williamboman/mason.nvim",          --
-  "williamboman/mason-lspconfig.nvim",--
-  "hrsh7th/cmp-buffer",   --
-  "hrsh7th/cmp-cmdline",  --
-  "hrsh7th/cmp-nvim-lsp", --
-  "hrsh7th/cmp-path",     --
-  "hrsh7th/cmp-vsnip",    --
-  "hrsh7th/nvim-cmp",     --
-  "hrsh7th/vim-vsnip",    --
-  "itchyny/lightline.vim",--
-  "jalvesaq/Nvim-R",      --
-  "mengelbrecht/lightline-bufferline",--
-  "navarasu/onedark.nvim",            -- Theme inspired by Atom
-  "neovim/nvim-lspconfig",            --
-  "nvim-lua/plenary.nvim",            -- Dependencie of nvim-telescope/telescope.nvim
-  "nvim-telescope/telescope.nvim",    --
-  "nvim-treesitter/nvim-treesitter",  --
-  "whonore/vim-sentencer",            -- One sentence per line and wrap long lines
+  "folke/which-key.nvim",              -- Display available keybindings
+  "github/copilot.vim",                -- AI code completion
+  "gruvbox-community/gruvbox",         -- Gruvbox colorscheme
+  "hrsh7th/cmp-buffer",                -- Buffer completion for nvim-cmp
+  "hrsh7th/cmp-cmdline",               -- Cmdline completion for nvim-cmp
+  "hrsh7th/cmp-nvim-lsp",              -- LSP completion for nvim-cmp
+  "hrsh7th/cmp-path",                  -- Path completion for nvim-cmp
+  "hrsh7th/cmp-vsnip",                 -- VSnip completion for nvim-cmp
+  "hrsh7th/nvim-cmp",                  -- Completion engine for Neovim
+  "hrsh7th/vim-vsnip",                 -- Snippet engine for Neovim
+  "itchyny/lightline.vim",             -- Lightweight statusline/tabline
+  "jalvesaq/Nvim-R",                   -- R language support
+  "mengelbrecht/lightline-bufferline", -- Bufferline for lightline
+  "navarasu/onedark.nvim",             -- Theme inspired by Atom
+  "neovim/nvim-lspconfig",             -- LSP configuration for Neovim
+  "nvim-lua/plenary.nvim",             -- Dependencie of nvim-telescope/telescope.nvim
+  "nvim-telescope/telescope.nvim",     -- Fuzzy finder for Neovim
+  "nvim-tree/nvim-web-devicons",       -- Icons for Neovim
+  "nvim-treesitter/nvim-treesitter",   -- Treesitter support for Neovim
+  "whonore/vim-sentencer",             -- One sentence per line and wrap long lines
+  "williamboman/mason-lspconfig.nvim", -- LSP configuration for Mason
+  "williamboman/mason.nvim",           -- LSP installer for Neovim
 })
 
 require("plugins.treesitter")
