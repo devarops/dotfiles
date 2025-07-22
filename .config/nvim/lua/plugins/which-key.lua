df3c0547d44fe7bc8f8d10c2fccbf45c464b9b1f
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-22 14:11:25 -0700

    🧑‍💻 Clarify buffer key descriptions and rename which-key variable
    Improve developer experience by using mnemonic descriptions in buffer keymaps and replacing the ambiguous "wk" with the more descriptive "which_key" for clarity and maintainability.

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,7 +1,7 @@
-local wk = require("which-key")
-wk.setup {}
-wk.add({
-  { "<leader>f", group = "[B]uffer navigation" },
+local which_key = require("which-key")
+which_key.setup {}
+which_key.add({
+  { "<leader>b", group = "[B]uffer navigation" },
   { "<leader>f", group = "[F]ind using Telescope" },
   { "<leader>p", group = "[P]rogramming environment" },
 })
