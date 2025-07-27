b537155adb9ebcbce397378e2697a648467d6907
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-27 11:27:25 -0700

    Add key maps for Futigive and Undotree

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,5 +2,6 @@ local which_key = require("which-key")
 which_key.setup {}
 which_key.add({
   { "<leader>f", group = "[F]ind using Telescope" },
+  { "<leader>g", group = "[G]it" },
   { "<leader>p", group = "[P]rogramming environment" },
 })
