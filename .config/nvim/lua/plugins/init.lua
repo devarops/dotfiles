8998dd3cd24c2601c5edabe47193c2331fa78e98
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-27 12:06:24 -0700

    Remove LSP plugins because Neovim version 0.11 now includes LSP by default

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,17 +18,9 @@ require("lazy").setup({
   "folke/which-key.nvim",              -- Display available keybindings
   "github/copilot.vim",                -- AI code completion
   "gruvbox-community/gruvbox",         -- Gruvbox colorscheme
-  "hrsh7th/cmp-buffer",                -- Buffer completion for nvim-cmp
-  "hrsh7th/cmp-cmdline",               -- Cmdline completion for nvim-cmp
-  "hrsh7th/cmp-nvim-lsp",              -- LSP completion for nvim-cmp
-  "hrsh7th/cmp-path",                  -- Path completion for nvim-cmp
-  "hrsh7th/cmp-vsnip",                 -- VSnip completion for nvim-cmp
-  "hrsh7th/nvim-cmp",                  -- Completion engine for Neovim
-  "hrsh7th/vim-vsnip",                 -- Snippet engine for Neovim
   "jalvesaq/Nvim-R",                   -- R language support
   "mbbill/undotree",                   -- Persistent undo history
   "navarasu/onedark.nvim",             -- Theme inspired by Atom
-  "neovim/nvim-lspconfig",             -- LSP configuration for Neovim
   "nvim-lua/plenary.nvim",             -- Dependencie of nvim-telescope/telescope.nvim
   "nvim-lualine/lualine.nvim",         -- Statusline for Neovim
   "nvim-telescope/telescope.nvim",     -- Fuzzy finder for Neovim
@@ -36,14 +28,10 @@ require("lazy").setup({
   "nvim-treesitter/nvim-treesitter",   -- Treesitter support for Neovim
   "tpope/vim-fugitive",                -- Git integration for Neovim
   "whonore/vim-sentencer",             -- One sentence per line and wrap long lines
-  "williamboman/mason-lspconfig.nvim", -- LSP configuration for Mason
-  "williamboman/mason.nvim",           -- LSP installer for Neovim
 })
 
-require("plugins.treesitter")
-
-require("plugins.cmp")
 require("plugins.copilot")
 require("plugins.lualine")
 require("plugins.sentencer")
+require("plugins.treesitter")
 require("plugins.which-key")
