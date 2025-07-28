900757986747eab958c0e385330803ef716ae4e9
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-27 22:13:35 -0700

    Fix typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,5 +3,5 @@ require("variables")   -- Define global variables (if any)
 require("plugins")     -- Load plugin manager and plugins
 require("lsp")         -- Setup LSP (which might define commands used in mappings)
 require("vimrc")       -- Any legacy settings from old vimrc
-require("kemaps")      -- Finally load keymaps, which depend on plugins and options
+require("keymaps")     -- Define key mappings
 
