83e8d3aef5d616e9397ea86db882b3f64f3eff19
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-27 21:49:25 -0700

    Add confog from: https://gpanders.com/blog/whats-new-in-neovim-0-11/

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,3 +1,13 @@
 vim.lsp.enable(
     'r_language_server'
 )
+
+-- Enable LSP complition
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
--- .config/nvim/lua/vimrc.lua
+++ .config/nvim/lua/vimrc.lua
@@ -1,6 +1,7 @@
 vim.cmd([[
     colorscheme gruvbox
     highlight ColorColumn ctermbg=238
+    set completeopt+=noselect
     set mouse=
     syntax on
 ]])
