397bb0460e9b10c8673c93cda6fa25e751f0c53c
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-27 22:11:24 -0700

    Mind the order of loading configurations becase kemaps were not working

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
-require("lsp")
-require("options")
-require("plugins")
-require("variables")
-require("vimrc")
+require("options")     -- Set options like mapleader
+require("variables")   -- Define global variables (if any)
+require("plugins")     -- Load plugin manager and plugins
+require("lsp")         -- Setup LSP (which might define commands used in mappings)
+require("vimrc")       -- Any legacy settings from old vimrc
+require("kemaps")      -- Finally load keymaps, which depend on plugins and options
+
