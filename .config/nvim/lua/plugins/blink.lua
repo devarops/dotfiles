6a247105aa34f5071eeb60d28f77e30b349a5b13
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-28 18:28:36 -0700

    Show documentation automaticaly

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -7,7 +7,6 @@ return {
     appearance = {
       nerd_font_variant = 'mono'
     },
-    completion = { documentation = { auto_show = false } },
     fuzzy = { implementation = "prefer_rust_with_warning" }
   },
 }
