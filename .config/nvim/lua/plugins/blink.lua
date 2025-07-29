7c9700d7bda1a7211dddaafa676989121cd33598
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-28 20:03:38 -0700

    Enable signature

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,13 +1,14 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono'
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+        keymap = { preset = 'default' },
+        appearance = {
+            nerd_font_variant = 'mono'
+        },
+        completion = { documentation = { auto_show = true } },
+        signature = { enabled = true },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
     },
-    completion = { documentation = { auto_show = true } },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
 }
