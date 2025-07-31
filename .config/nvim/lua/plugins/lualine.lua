f34ecc49d976168e6062cf7de9be554ea507fb6e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 19:24:50 -0700

    Customize buffer colors in bufferline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,16 @@
-require('lualine').setup {
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   tabline = {
     lualine_a = {
       {
         'buffers',
         mode = 2,
+        buffers_color = {
+          active = { fg = 'green', bg = 'colour238'},
+          inactive = { fg = 'colour238', bg = 'green' },
+        },
       },
     },
   },
 }
-
