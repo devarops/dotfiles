6c82fab67275c005c4430c5f8c734bf54a34554e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 21:19:24 -0700

    Coolor buffers in buffer line

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,10 @@ return {
         {
           'buffers',
           mode = 2,
+          buffers_color = {
+            active = { fg = 'green', bg = 'colour238'},
+            inactive = { fg = 'colour238', bg = 'green' },
+          },
         },
       },
     },
