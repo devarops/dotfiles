06d4bf0dcbbb9b82db92213cd54de6fc25532b5e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 20:16:17 -0700

    Remove configuration that is not working

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,15 +1,10 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
   tabline = {
     lualine_a = {
       {
         'buffers',
         mode = 2,
-        buffers_color = {
-          active = { fg = 'green', bg = 'colour238'},
-          inactive = { fg = 'colour238', bg = 'green' },
-        },
       },
     },
   },
