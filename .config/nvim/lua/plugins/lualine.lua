34bc6314abf8ea2b3fb643290ffbdbe1647c1376
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 21:33:29 -0700

    Change colors

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,15 @@
+local colors = {
+  black        = '#282828',
+  white        = '#ebdbb2',
+  red          = '#fb4934',
+  green        = '#b8bb26',
+  blue         = '#83a598',
+  yellow       = '#fe8019',
+  gray         = '#a89984',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+}
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
@@ -8,8 +20,8 @@ return {
           'buffers',
           mode = 2,
           buffers_color = {
-            active = { fg = 'green', bg = 'colour238'},
-            inactive = { fg = 'colour238', bg = 'green' },
+            active = { fg = color.gray, bg = colors.green },
+            inactive = { fg = colors.gree , bg = colors.inactivegray },
           },
         },
       },
