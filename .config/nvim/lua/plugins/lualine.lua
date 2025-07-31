5c14e259ee970fee79c38d608cc60957819e02a9
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 22:11:20 -0700

    Change color forlualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,36 +1,15 @@
-local colors = {
-  vertsplit      = '#181A1F',
-  special_grey   = '#3B4048',
-  menu_grey      = '#3E4452',
-  cursor_grey    = '#2C323C',
-  gutter_fg_grey = '#4B5263',
-  blue           = '#82b1ff',
-  dark_red       = '#BE5046',
-  white          = '#bfc7d5',
-  green          = '#C3E88D',
-  purple         = '#c792ea',
-  yellow         = '#ffcb6b',
-  light_red      = '#ff869a',
-  red            = '#ff5370',
-  dark_yellow    = '#F78C6C',
-  cyan           = '#89DDFF',
-  comment_grey   = '#697098',
-  black          = '#292D3E',
-}
-
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
   opts = {
-    theme  = 'palenight',
     tabline = {
       lualine_a = {
         {
           'buffers',
           mode = 2,
           buffers_color = {
-            active = { fg = colors.black, bg = colors.green, gui = 'bold' },
-            inactive = { fg = colors.green, bg = colors.menu_grey },
+            active = { fg = '#30302c', bg = '#99ad6a', gui = 'bold' },
+            inactive = { fg = '#e8e8d3', bg = '#4e4e43' },
           },
         },
       },
