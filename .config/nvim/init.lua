07e13e805ecc7186ff27d8b0c94f449c2e8d5425
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 17:51:26 -0700

    Use 'config/lazy.lua' instead of 'plugins/init.lua'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 require("options")     -- Set options like mapleader
 require("variables")   -- Define global variables (if any)
-require("plugins")     -- Load plugin manager and plugins
+require("config.lazy") -- Lazy load plugins using lazy.nvim
 require("lsp")         -- Setup LSP (which might define commands used in mappings)
 require("vimrc")       -- Any legacy settings from old vimrc
 require("keymaps")     -- Define key mappings
-
