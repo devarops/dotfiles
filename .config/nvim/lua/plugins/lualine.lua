2944700a0a2f6c5753b92ae3416ce11885b90274
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2025-07-30 22:14:56 -0700

    Change lualine colors

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
           'buffers',
           mode = 2,
           buffers_color = {
-            active = { fg = '#30302c', bg = '#99ad6a', gui = 'bold' },
-            inactive = { fg = '#e8e8d3', bg = '#4e4e43' },
+            active = { fg = '#30302c', bg = '#99ad6a' },
+            inactive = { fg = '#99ad6a', bg = '#4e4e43' },
           },
         },
       },
