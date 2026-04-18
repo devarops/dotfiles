7ed216c20a564ce7ec3f7d50911f8f259c0dc630
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-04-17 19:30:04 -0700

    🚚 Move plenary dependency into telescope plugin file
    Deleted standalone plenary.lua and merged it into telescope.lua
    following the same pattern as blink.lua and oil.nvim.

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,2 +1,5 @@
 -- Fuzzy finding and searching in Neovim
-vim.pack.add({ 'https://github.com/nvim-telescope/telescope.nvim' })
+vim.pack.add({
+  'https://github.com/nvim-lua/plenary.nvim',         -- dependency
+  'https://github.com/nvim-telescope/telescope.nvim', -- main plugin
+})
